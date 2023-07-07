bcacbc90a7eb8e484ae0311f8ac40ecddfe30240
Vlad <vladyslavhrebennikov@gmail.com>
2023-07-07 17:33:42 +0300

    termPromo

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,8 +10,15 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $orders = Order::where('payment_status', 'paid')->orWhere('payment_status', 'cash')
-        ->where('delivery_status', 'preparing')->orderBy('created_at', 'desc')->get();
+        // $orders = Order::where('payment_status', 'paid')->orWhere('payment_status', 'cash')
+        // ->where('delivery_status', 'preparing')->orWhere()->orderBy('created_at', 'desc')->get();
+        $orders = Order::where(function ($query) {
+            $query->where('payment_status', 'paid')
+                ->orWhere('payment_status', 'cash');
+        })
+        ->where('delivery_status', 'preparing')
+        ->orderBy('created_at', 'desc')
+        ->get();
 
         return view('admin.dashboard.index',[
             'orders' => $orders,
